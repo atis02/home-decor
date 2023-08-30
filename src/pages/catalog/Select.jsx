7c131ed1d5647db06696f309a7b4c7eb50be0c6a
atis02 <guwancikramow@gmail.com>
2023-08-30 16:44:34 +0500

    new message

--- src/pages/catalog/Select.jsx
+++ src/pages/catalog/Select.jsx
@@ -1,116 +1,22 @@
 import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import React from 'react'
+import {Data} from './bselect.mjs'
 export default function BasicSelect() {
+  
     return (
       <>
+      {Data.map((item)=>(
         <Accordion>
-          <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
-            <Typography>Что происходит после оформления заказа?</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-            <Typography>Время работы</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-            <Typography>Как происходит оплата</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-            <Typography>Доставка</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-            <Typography>Сроки</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-            <Typography>Упаковка</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-            <Typography>На какой бумаге печать</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-            <Typography>Какие рамы мы продаем</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary aria-controls="panel2a-content" id="panel2a-header">
-            <Typography>Паспoрту</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
+        <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
+          <Typography>{item.name}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+           {item.title}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      ))}
       </>
     );
   }
---
+++ src/pages/catalog/bselect.mjs
@@ -0,0 +1,38 @@
+export const Data =[
+    {
+      name:'Что происходит после оформления заказа?',
+      title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget'
+    },
+    {
+      name:'Время работы',
+      title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget'
+    },
+    {
+      name:'Как происходит оплата',
+      title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget'
+    },
+    {
+      name:'Доставка',
+      title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget'
+    },
+    {
+      name:'Сроки',
+      title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget'
+    },
+    {
+      name:'Упаковка',
+      title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget'
+    },
+    {
+      name:'На какой бумаге печать',
+      title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget'
+    },
+    {
+      name:'Какие рамы мы продаем',
+      title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget'
+    },
+    {
+      name:'Паспoрту',
+      title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget'
+    },
+  ]
